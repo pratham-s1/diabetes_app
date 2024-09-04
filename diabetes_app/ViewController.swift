3b4d1d200d7d6f1df0bccd1e7f4de6742ca2843a
prathamsingh <singhpratham892@gmail.com>
2024-09-04 19:35:57 +0530

    Initial Commit

---
+++ diabetes_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diabetes_app
+//
+//  Created by Pratham on 04/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
